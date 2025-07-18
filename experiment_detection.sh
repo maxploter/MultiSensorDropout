232a34f4d4fe21ff0853989bda4f6071d353a674
Maksim Ploter <maksim.ploter@gmail.com>
2025-07-18 18:59:02 +0300

    chore: assign port random

--- experiment_detection.sh
+++ experiment_detection.sh
@@ -60,7 +60,8 @@ master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
 
-export MASTER_PORT=12381
+export MASTER_PORT=$((12000 + RANDOM % 1000))
+echo "MASTER_PORT="$MASTER_PORT
 
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
 echo "WORLD_SIZE="$WORLD_SIZE
