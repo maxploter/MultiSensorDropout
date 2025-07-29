7bcf65e636456fc01b89a1e2c5b1b81ffaaaa676
Maksim Ploter <maksim.ploter@gmail.com>
2025-07-29 20:29:47 +0300

    fix: setup throw on non-cuda

--- models/ops/setup.py
+++ models/ops/setup.py
@@ -44,7 +44,7 @@ def get_extensions():
             "-D__CUDA_NO_HALF2_OPERATORS__",
         ]
     else:
-        print("CUDA is not available. Building CPU-only version.")
+        raise NotImplementedError('Cuda is not availabel')
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
     include_dirs = [extensions_dir]
